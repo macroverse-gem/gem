35a7cd77a003af9b347ffbd13247a2c46f4a3f32
macroverse-project <96976076+macroverse-project@users.noreply.github.com>
2024-10-13 21:50:09 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const QUARRY_CONTRACT = "0x1a45d43d34D004036d99520E79AA1130eAc37Bd8";
 export const GEM_CONTRACT = "0x3a3cCc5089c6E6E93ebE438a0a680Cea7Af310c9";
-export const USDT_CONTRACT = "0x7ef95a0fee0dd31b22626fa2e10ee6a223f8a684";
+export const USDT_CONTRACT = "0x5cf0869e4305500b985b64fd552b960f01163840";
 export const RPC_URL_97 = `https://data-seed-prebsc-1-s2.binance.org:8545/`;
 
 export const ABI = {
